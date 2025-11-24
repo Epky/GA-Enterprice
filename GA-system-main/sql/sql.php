fd9c2aa655f71cb6257fe3528c9d8dca09ba9576
epky <payanedsel@gmail.com>
2025-11-25 01:30:05 +0800

    deleted

--- GA-system-main/sql/sql.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$connsn = "localhost";
-	$connun = "root";
-	$connpass = "";
-	$conndbn = "store";
-	$connport = "3306";
-	$conn = mysqli_connect($connsn,$connun,$connpass,$conndbn,$connport);
-	//error_reporting(0);
- ?>
